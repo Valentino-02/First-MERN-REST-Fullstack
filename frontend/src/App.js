5463fcc1eff36fc9c8cadcda0a22566ab8b52530
Valentino <collazo.valentino@gmail.com>
2022-05-09 11:30:23 -0300

    Goals working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Header from './components/Header';
-import Dashboard from './pages/Dashboard';
-import Login from './pages/Login';
-import Register from './pages/Register';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Header from './components/Header'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return (
@@ -14,12 +15,13 @@ function App() {
           <Routes>
             <Route path='/' element={<Dashboard />} />
             <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />          
+            <Route path='/register' element={<Register />} />
           </Routes>
         </div>
       </Router>
+      <ToastContainer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,58 +1,94 @@
-import { useState, useEffect } from 'react';
-import { FaSignInAlt } from 'react-icons/fa';
+import { useState, useEffect } from 'react'
+import { FaSignInAlt } from 'react-icons/fa'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { login, reset } from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 function Login() {
   const [formData, setFormData] = useState({
     email: '',
     password: '',
-  });
+  })
 
-  const { email, password } = formData;
+  const { email, password } = formData
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  )
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+
+    if (isSuccess || user) {
+      navigate('/')
+    }
+
+    dispatch(reset())
+  }, [user, isError, isSuccess, message, navigate, dispatch])
 
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     }))
   }
 
   const onSubmit = (e) => {
     e.preventDefault()
+
+    const userData = {
+      email,
+      password,
+    }
+
+    dispatch(login(userData))
+  }
+
+  if (isLoading) {
+    return <Spinner />
   }
 
   return (
     <>
-      <section className="heading">
+      <section className='heading'>
         <h1>
           <FaSignInAlt /> Login
         </h1>
-        <p>Please login with your account</p>
+        <p>Login and start setting goals</p>
       </section>
 
-      <section className="form">
+      <section className='form'>
         <form onSubmit={onSubmit}>
           <div className='form-group'>
             <input
-              type="email"
-              className="form-control"
-              id="email"
-              name="email"
+              type='email'
+              className='form-control'
+              id='email'
+              name='email'
               value={email}
-              placeholder="Enter your email"
+              placeholder='Enter your email'
               onChange={onChange}
             />
-          </div> 
+          </div>
           <div className='form-group'>
             <input
-              type="password"
-              className="form-control"
-              id="password"
-              name="password"
+              type='password'
+              className='form-control'
+              id='password'
+              name='password'
               value={password}
-              placeholder="Enter your password"
+              placeholder='Enter password'
               onChange={onChange}
             />
-          </div> 
+          </div>
+
           <div className='form-group'>
             <button type='submit' className='btn btn-block'>
               Submit
@@ -61,7 +97,7 @@ function Login() {
         </form>
       </section>
     </>
-  );
+  )
 }
 
-export default Login;
+export default Login
