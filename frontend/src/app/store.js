9caf6c822572ed6e1cd3a7195220aa2654c9ee5d
Valentino <collazo.valentino@gmail.com>
2022-05-10 19:11:02 -0300

    small fix

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,14 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../features/auth/authSlice'
-import goalReducer from '../features/goals/goalSlice'
 import favoriteReducer from '../features/favorites/favoriteSlice'
 import characterReducer from '../features/characters/characterSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    goals: goalReducer,
     favorites: favoriteReducer,
     character: characterReducer,
-  },
+  },  
 })
