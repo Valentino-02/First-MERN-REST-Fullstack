b123b0c7db24aea19d3d2f2148fd10cd501f0913
Valentino <collazo.valentino@gmail.com>
2022-07-03 16:57:22 -0300

    deploy fix

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,40 @@
-const path = require('path')
-const express = require('express')
-const colors = require('colors')
-const dotenv = require('dotenv').config()
-const { errorHandler } = require('./middleware/errorMiddleware')
-const connectDB = require('./config/db')
-const res = require('express/lib/response')
-const port = process.env.PORT || 4000
+const path = require("path");
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const res = require("express/lib/response");
+const port = process.env.PORT || 4000;
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/users', require('./routes/userRoutes'))
-app.use('/api/character', require('./routes/characterRoutes'))
-app.use('/api/planet', require('./routes/planetRoutes'))
-app.use('/api/film', require('./routes/filmRoutes'))
-app.use('/api/favorite', require('./routes/favoriteRoutes'))
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/character", require("./routes/characterRoutes"));
+app.use("/api/planet", require("./routes/planetRoutes"));
+app.use("/api/film", require("./routes/filmRoutes"));
+app.use("/api/favorite", require("./routes/favoriteRoutes"));
 
 // Serve frontend
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-    app.get('*', (req, resp) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+  app.get("*", (req, resp) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
 } else {
-    app.get('/', (req, res) => res.send('Please set to production'))
+  app.get("/", (req, res) => res.send("Please set to production"));
 }
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-app.listen(port)
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}!`);
+});
