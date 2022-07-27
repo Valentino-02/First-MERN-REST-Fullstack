40c13a35619f196da8022075add5772b8addbda5
Valentino <collazo.valentino@gmail.com>
2022-07-27 14:07:37 -0300

    some fixes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
-const path = require("path");
 const express = require("express");
+const path = require("path");
+
 const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
+
 const res = require("express/lib/response");
+
 const port = process.env.PORT || 4000;
 
 connectDB();
@@ -22,12 +25,10 @@ app.use("/api/favorite", require("./routes/favoriteRoutes"));
 
 // Serve frontend
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.use(express.static(path.resolve(__dirname, "../frontend/build")));
 
-  app.get("*", (req, resp) =>
-    res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html")
-    )
+  app.get("*", (req, res) =>
+    resp.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => res.send("Please set to production"));
